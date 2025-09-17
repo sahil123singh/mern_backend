a3441233e34cc571b5d5b5f58c325c2da6e52199
Sahil Singh <sahilsingh20698@gmail.com>
2025-09-17 15:30:25 +0530

    changes in index

--- config/v1/config.js
+++ config/v1/config.js
@@ -7,11 +7,11 @@ CONFIG.port = process.env.APPLICATION_PORT || '4000';
 CONFIG.api_ver = process.env.API_VER;
 
 if (process.env.ENV === 'prod' || process.env.ENV === 'production') {
-    CONFIG.cors_whitelist = ['http://localhost:4000', 'http://localhost:3000','http://10.242.213.8:3000','http://10.242.213.8:4000']
+    CONFIG.cors_whitelist = ['http://localhost:4000', 'http://localhost:3000', 'http://10.242.213.8:3000', 'http://10.242.213.8:4000', 'https://mern-frontend-beta-one.vercel.app']
 } else if (process.env.ENV === 'stag' || process.env.ENV === 'staging') {
-    CONFIG.cors_whitelist = ['http://localhost:4000', 'http://localhost:3000','http://10.242.213.8:3000', 'http://10.242.213.8:4000']
+    CONFIG.cors_whitelist = ['http://localhost:4000', 'http://localhost:3000', 'http://10.242.213.8:3000', 'http://10.242.213.8:4000', 'https://mern-frontend-beta-one.vercel.app']
 } else {
-    CONFIG.cors_whitelist = ['http://localhost:4000', 'http://localhost:3000','http://10.242.213.8:3000', 'http://10.242.213.8:4000']
+    CONFIG.cors_whitelist = ['http://localhost:4000', 'http://localhost:3000', 'http://10.242.213.8:3000', 'http://10.242.213.8:4000', 'https://mern-frontend-beta-one.vercel.app']
 }
 
 module.exports = CONFIG;
